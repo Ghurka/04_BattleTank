faa39492d64b052e62a2dbe46952c2133900dc31
Marco Capo <marcojcapo@gmail.com>
2017-09-02 20:45:12 -0300

    Poiting with barrel

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -9,4 +9,9 @@
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called at speed: %s"), DegreesPerSecond );
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreePerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
